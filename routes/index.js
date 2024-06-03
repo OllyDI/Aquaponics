fd408b8f415972660f5e1a8d09f81d22696ed7f4
OllyDI <choiseungho11@hanmail.net>
2024-06-03 19:53:48 +0900

    24.06.03

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,13 @@ var path = require("path")
 router.get('/', function(req, res, next) {
   res.sendFile(path.join(__dirname, "../views/index.html"));
 });
+
+router.get('/members/login', function(req, res, next) {
+  res.sendFile(path.join(__dirname, "../views/members/login.html"));
+});
+
+router.get('/members/register', function(req, res, next) {
+  res.sendFile(path.join(__dirname, "../views/members/register.html"))
+});
+
 module.exports = router;
