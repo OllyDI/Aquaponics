4b30463f828caabf70805f962f9bce9ef18f5650
OllyDI <choiseungho11@hanmail.net>
2024-06-04 22:14:15 +0900

    24.06.04

--- app.js
+++ app.js
@@ -4,10 +4,15 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+var expressSession = require('express-session');
+var passport = require('passport')
+var localStrategy = require('passport-local').Strategy;
+
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+// var usersRouter = require('./routes/users');
 
 var app = express();
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -20,7 +25,94 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+// app.use('/users', usersRouter);
+
+app.use(cookieParser());
+app.use(
+  expressSession({
+    secret: "aqufarm",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
+app.post('/session/login', 
+  passport.authenticate('local', {successRedirect: '/', failureRedirect: '/members/login' }
+));
+
+
+passport.use(new localStrategy(
+  {
+    uid: 'id',
+    upw: 'pw'
+  },
+  function(username, password, done) {
+    User.findOne({ username: username }, function (err, user) {
+      // 검사 중 에러
+      if (err) { return done(err); }
+
+      // 아이디 에러
+      if (!user) {
+        return done(null, false, { message: 'Incorrect username.' });
+      }
+
+      // 패스워드 에러
+      if (!user.validPassword(password)) {
+        return done(null, false, { message: 'Incorrect password.' });
+      }
+
+      //성공
+      return done(null, user);
+    });
+  }
+));
+
+
+passport.use(new LocalStrategy(
+	//verify callback 함수
+  function(username, password, done) {
+    User.findOne({ username: username }, function (err, user) {
+      if (err) { return done(err); }
+
+			// 아이디 없음
+      if (!user) {
+        return done(null, false, { message: 'Incorrect username.' });
+      }
+
+			// 비밀번호 틀림
+      if (!user.validPassword(password)) {
+        return done(null, false, { message: 'Incorrect password.' });
+      }
+
+			// 성공
+      return done(null, user);
+    });
+  }
+));
+
+// 로그인 성공 시 유저 정보 세션에 저장
+passport.serializeUser(function(user, done) {
+  done(null, user,id);
+});
+// 로그인 이후 검증 필요한 페이지마다 검사
+passport.deserializeUser(function(id, done) {
+  User.findById(id, function(err, user) {
+    done(err, user);
+  });
+});
+
+
+app.get('/session/logout', function(req, res) {
+  req.logout();
+  req.session.save((err) => {
+    res.redirect('/members/login');
+  })
+})
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ public/js/duplicate.js
@@ -0,0 +1,41 @@
+$(document).ready(function () {
+    document.querySelector('form').addEventListener('submit', function(e){
+        if(document.getElementById('email').value == '') {
+            e.preventDefault(); // 폼 전송 막는 함수
+            $("#lEmail").css("color", "red").text("아이디를 입력하세요.");
+        }
+        if(document.getElementById('pw').value == '') {
+            e.preventDefault();
+            $("#lPw").css("color", "red").text("비밀번호를 입력하세요.");
+        } else $("#lPw").text("");
+        if(document.getElementById('userName').value == '') {
+            e.preventDefault();
+            $("#lName").css("color", "red").text("닉네임을 입력하세요.");
+        } else $("#lName").text("");
+    });
+
+
+    // $("#email").on("focusout", function (e) {
+    //     var id = $("#email").val();
+    //     if (id == '' || id.length == 0) { return false; }
+
+    //     //Ajax로 전송
+    //     $.ajax({
+    //         url: '/duplicate',
+    //         data: {
+    //             email: id
+    //         },
+    //         type: 'POST',
+    //         datatype: 'json',
+    //         success: function (result) {
+    //             if (result == true) {
+    //                 $("#reg").attr("type", "submit");
+    //                 $("#lEmail").css("color", "black").text("사용 가능한 ID 입니다.");
+    //             } else {
+    //                 $("#reg").attr("type", "button");
+    //                 $("#lEmail").css("color", "red").text("사용 불가능한 ID 입니다.");
+    //             }
+    //         }
+    //     });
+    // });
+})
