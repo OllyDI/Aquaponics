e7cd80d887953862d08ae61b52671a820e15c93a
OllyDI <choiseungho11@hanmail.net>
2024-07-01 21:24:06 +0900

    24.07.01

--- public/js/duplicate.js
+++ public/js/duplicate.js
@@ -1,51 +1,5 @@
 $(document).ready(function () {
-    document.querySelector('form').addEventListener('submit', function(e){
-        if(document.getElementById('level').value == '') {
-            e.preventDefault(); // 폼 전송 막는 함수
-            $("#lLevel").attr("hidden", false)
-        } else $("#lLevel").attr("hidden", true)
-
-        if(document.getElementById('school').value == '') {
-            e.preventDefault();
-            $("#lSchool").attr("hidden", false)
-        } else $("#lSchool").attr("hidden", true)
-
-        if(document.getElementById('uname').value == '') {
-            e.preventDefault();
-            $("#lUname").attr("hidden", false)
-        } else $("#lUname").attr("hidden", true)
-
-        if(document.getElementById('grade').value == '') {
-            e.preventDefault();
-            $("#lUgrade").attr("hidden", false)
-        } else $("#lUgrade").attr("hidden", true)
-
-        if(document.getElementById('class').value == '') {
-            e.preventDefault();
-            $("#lUclass").attr("hidden", false)
-        } else $("#lUclass").attr("hidden", true)
-
-        if(document.getElementById('number').value == '') {
-            e.preventDefault();
-            $("#lUnumber").attr("hidden", false)
-        } else $("#lUnumber").attr("hidden", true)
-
-        if(document.getElementById('uid').value == '') {
-            e.preventDefault();
-            $("#lUid").attr("hidden", false)
-        } else $("#lUpw").attr("hidden", true)
-
-        if(document.getElementById('upw').value == '') {
-            e.preventDefault();
-            $("#lUpw").attr("hidden", false)
-        } else $("#lUpw").attr("hidden", true)
-
-        
-    });
-
-
     $("#uid").on("focusout", function (e) {
-        console.log("focus")
         var id = $("#uid").val();
         var level = $("#level").val();
         if (id == '' || id.length == 0) { return false; }
@@ -59,16 +13,13 @@ $(document).ready(function () {
                 level: level
             },
             datatype: 'json',
-            success: function (res) {
-                console.log(res)
-                if (res == true) {
+            success: function (data) {
+                if (data == true) {
                     $("#lUid").attr("hidden", true)
                     $("#reg").attr("type", "submit");
-                    $("#lUid").css("color", "black").text("사용 가능한 ID 입니다.");
                 } else {
                     $("#lUid").attr("hidden", false)
                     $("#reg").attr("type", "button");
-                    $("#lUid").css("color", "red").text("사용 불가능한 ID입니다.");
                 }
             }
         });
